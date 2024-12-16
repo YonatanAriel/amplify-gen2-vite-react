3af9240a0eb83f29b2f8306ede839871c0125e3e
Yonatan Ariel <yonatanriel@gmail.com>
2024-12-16 08:42:45 +0200

    adding oauth2 by google to the authentication process

--- amplify/auth/resource.ts
+++ amplify/auth/resource.ts
@@ -1,7 +1,22 @@
-import { defineAuth } from "@aws-amplify/backend";
+import { defineAuth, secret } from "@aws-amplify/backend";
 
 export const auth = defineAuth({
   loginWith: {
     email: true,
+    externalProviders: {
+      google: {
+        clientId: secret("GOOGLE_CLIENT_ID"),
+        clientSecret: secret("GOOGLE_CLIENT_SECRET"),
+        scopes: ["email"],
+      },
+      callbackUrls: [
+        "http://localhost:5173/",
+        "https://main.dlwvra5zr7y2a.amplifyapp.com/",
+      ],
+      logoutUrls: [
+        "http://localhost:5173/",
+        "https://main.dlwvra5zr7y2a.amplifyapp.com/",
+      ],
+    },
   },
 });
